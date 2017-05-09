ff9fb54cc8ce6877417f3d1bd39233ec2dd4fdb4
Andreas Stöckel <astoecke@uwaterloo.ca>
2017-05-09 18:19:42 -0400

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='nengo_conductance_synapses',
+    packages=['nengo_conductance_synapses'],
+    version='0.1',
+    author='Andreas Stöckel',
+    description='Conductance based synapses in Nengo',
+    url='https://github.com/ctn-waterloo/nengo_conductance_synapses',
+    license='GPLv3',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering',
+        ]
+    )
