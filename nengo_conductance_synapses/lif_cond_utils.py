faa1a7bd34c6bfe386a88d0819333bca07f4dd7f
Andreas Stöckel <astoecke@uwaterloo.ca>
2017-06-09 18:59:56 -0400

    WIP, implement more methods for calculation of scale_E, scale_I

--- nengo_conductance_synapses/lif_cond_utils.py
+++ nengo_conductance_synapses/lif_cond_utils.py
@@ -36,14 +36,13 @@ def _rate(g_tot, e_eq, tau_ref=2e-3, v_th=1.0):
     return mask / (tau_ref + t_spike)
 
 
-def lif_cond_calc_g_tot_e_eq(gL, gE, gI, e_rev_E=4.33, e_rev_I=-0.33):
-    g_tot = gL + gE + gI
-    e_eq = (gE * e_rev_E + gI * e_rev_I) / g_tot
-    return g_tot, e_eq
-
-
-def lif_cond_rate(gL, gE, gI, e_rev_E=4.33, e_rev_I=-0.33,
-                  tau_ref=2e-3, v_th=1.0):
+def lif_cond_rate(gL,
+                  gE,
+                  gI,
+                  e_rev_E=4.33,
+                  e_rev_I=-0.33,
+                  tau_ref=2e-3,
+                  v_th=1.0):
     """
     Calculates the firing rate of a LIF neuron with conductance based synapses.
     Input to the model are conductances gE and gI. All parameters may be numpy
@@ -61,8 +60,9 @@ def lif_cond_rate(gL, gE, gI, e_rev_E=4.33, e_rev_I=-0.33,
     # Clamp gE, gI to non-negative values
     gE, gI = np.maximum(0, gE), np.maximum(0, gI)
 
-    # Calculate the equilibrium potential
-    g_tot, e_eq = lif_cond_calc_g_tot_e_eq(gL, gE, gI, e_rev_E, e_rev_I)
+    # Calculate the total conductance and the equilibrium potential
+    g_tot = gL + gE + gI
+    e_eq = (gE * e_rev_E + gI * e_rev_I) / g_tot
 
     return _rate(g_tot, e_eq, tau_ref, v_th)
 
@@ -86,8 +86,75 @@ def lif_rate(gL, J, tau_ref=2e-3, v_th=1.0):
     return _rate(gL, e_eq, tau_ref, v_th)
 
 
-def optimize_scale_E_scale_I_bias_E_bias_I(
-        x_intercept, max_rate, gL=50, e_rev_E=4.33, e_rev_I=-0.33, tau_ref=2e-3):
+def calc_gE_for_rate(rate,
+                     gL,
+                     gI,
+                     e_rev_E=4.33,
+                     e_rev_I=-0.33,
+                     tau_ref=2e-3,
+                     atol=1e-9):
+    """
+    Calculates the excitatory conductivity required to achive a certain spike
+    rate given an inhibitory conductivity.
+
+    gL: leak conductance.
+    gI: inhibitory conductance.
+    e_rev_E: excitatory synapse reversal potential.
+    e_rev_I: inhibitory synapse reversal potential.
+    tau_ref: refractory period in second.
+    """
+
+    # Convert the rate to a time-to-spike s
+    s = 1 / rate - tau_ref
+
+    # Shorthands
+    EE = e_rev_E
+    EI = e_rev_I
+    gIs = np.atleast_1d(gI)
+
+    # Solve t_spike_cond for gI using Newton's method
+    x = np.ones(gIs.shape) * 1000
+    for i, gI in enumerate(gIs):
+        step = 1
+        while np.abs(step) > atol:
+            e = np.exp(-s * (gL + gI + x[i]))
+            f = (x[i] * EE + gI * EI) * e - x[i] * EE - gI * EI + x[i] + gI + gL
+            df = (-x[i] * EE * s - gI * EI * s + EE) * e - EE + 1
+
+            step = f / df
+            x[i] = x[i] - step
+    return x
+
+
+def calc_gI_for_rate(rate, gL, gE, e_rev_E=4.33, e_rev_I=-0.33, tau_ref=2e-3):
+    """
+    Calculates the inhibitory conductivity required to achive a certain spike
+    rate given an inhibitory conductivity.
+
+    gL: leak conductance.
+    gE: excitatory conductance.
+    e_rev_E: excitatory synapse reversal potential.
+    e_rev_I: inhibitory synapse reversal potential.
+    tau_ref: refractory period in second.
+    """
+
+    # Just swap e_rev_E and e_rev_I and call the previous function.
+    return calc_gE_for_rate(rate, gL, gE, e_rev_I, e_rev_E, tau_ref)
+
+
+def calc_gE_for_intercept(gL, gI, e_rev_E=4.33, e_rev_I=-0.33):
+    """
+    Calculates the excitatory conductivity required to
+    """
+    return (gI * e_rev_I - gI - gL) / (1 - e_rev_E)
+
+
+def optimize_scale_E_scale_I_bias_E_bias_I(x_intercept,
+                                           max_rate,
+                                           gL=50,
+                                           e_rev_E=4.33,
+                                           e_rev_I=-0.33,
+                                           tau_ref=2e-3):
     """
     Optimizes scale_E, scale_I, bias_E, and bias_I for the given x_intercept and
     max_rate.
@@ -106,8 +173,8 @@ def optimize_scale_E_scale_I_bias_E_bias_I(
     EI = e_rev_I
 
     t_spike_tar = 1 / max_rate - tau_ref
-    assert np.all(
-        t_spike_tar > 0), "max_rate must be smaller than the inverse of tau_ref"
+    assert np.all(t_spike_tar >
+                  0), "max_rate must be smaller than the inverse of tau_ref"
 
     def optimize_single(xi, t_tar):
         def calc_a(b, c, d):
@@ -129,7 +196,8 @@ def optimize_scale_E_scale_I_bias_E_bias_I(
                 t = -np.log1p(-1 / e_eq) / g_tot
 
             lambda_ = 1e-5
-            return 1 / lambda_ * (t - t_tar) ** 2 + lambda_ * (a * c + b * d)
+            print(xi, b, c, d, t, t_tar)
+            return 1 / lambda_ * (t - t_tar)**2  #+ lambda_ * (a * c + b * d)
 
         bounds = [(0, 100), (1, 100), (1, 100)]
         x, _, _ = scipy.optimize.fmin_l_bfgs_b(
@@ -173,8 +241,9 @@ def calc_scale_E(scale_I, gL, x_intercept, e_rev_E=4.33, e_rev_I=-0.33):
     For a given scale_I, gL, and x_intercept calculates the corresponding
     scale_E.
     """
-    return (scale_I * (e_rev_I * x_intercept - e_rev_I - x_intercept +
-                       1) + gL) / (e_rev_E * x_intercept + e_rev_E - x_intercept - 1)
+    return (scale_I *
+            (e_rev_I * x_intercept - e_rev_I - x_intercept + 1) + gL) / (
+                e_rev_E * x_intercept + e_rev_E - x_intercept - 1)
 
 
 def calc_scale_I(scale_E, gL, x_intercept, e_rev_E=4.33, e_rev_I=-0.33):
@@ -182,37 +251,66 @@ def calc_scale_I(scale_E, gL, x_intercept, e_rev_E=4.33, e_rev_I=-0.33):
     For a given scale_E, gL, and x_intercept calculates the corresponding
     scale_I.
     """
-    return (scale_E * (e_rev_E * x_intercept + e_rev_E - x_intercept -
-                       1) - gL) / (e_rev_I * x_intercept + e_rev_I - x_intercept + 1)
+    return (scale_E *
+            (e_rev_E * x_intercept + e_rev_E - x_intercept - 1) - gL) / (
+                e_rev_I * x_intercept + e_rev_I - x_intercept + 1)
 
 
-def max_rate(scale_E, gL, e_rev_E=4.33, e_rev_I=-0.33, tau_ref=2e-3, v_th=1.0):
+def calc_scale_E_scale_I_bias_I(x_intercept,
+                                max_rate,
+                                gL,
+                                e_rev_E=4.33,
+                                e_rev_I=-0.33,
+                                tau_ref=2e-3):
+    EE = e_rev_E
+    EI = e_rev_I
+    xi = x_intercept
+
+    scale_I = -1
+    gI = 0
+#    while scale_I < 0:
+    gEmax = calc_gE_for_rate(max_rate, gL, gI, e_rev_E, e_rev_I, tau_ref)
+
+    sI = xi * EI - EI - xi + 1
+    sE = xi * EE + EE - xi - 1
+
+    scale_E = gEmax / 2
+    scale_I = (sE * gEmax + 2 * (EI * gI - gI - gL)) / (2 * sI)
+    bias_I = (sE * gEmax + 2 * (gI * xi *
+                                (EI - 1) - gL)) / (sE * gEmax + 2 *
+                                                   (gI * (EI - 1) - gL))
+
+#        gI = gI + 10
+
+    return scale_E, scale_I, bias_I
+
+
+def solve_max_rate_x_intercept(x_intercept,
+                               max_rate,
+                               gL=None,
+                               e_rev_E=4.33,
+                               e_rev_I=-0.33,
+                               tau_ref=2e-3,
+                               v_th=1.0):
     """
-    Calculates the maximum rate (rate at x=1) of a neuron with conductance based
-    neurons for the given scale_E and gL.
-    """
-    g_tot, e_eq = lif_cond_calc_e_eq_and_g_tot(
-        gL, 2 * scale_E, 0, e_rev_E, e_rev_I)
-    return _rate(g_tot, e_eq, tau_ref=2e-3, v_th=1.0)
-
-
-def solve_max_rate_x_intercept(x_intercept, max_rate, gL=None, e_rev_E=4.33,
-                               e_rev_I=-0.33, tau_ref=2e-3, v_th=1.0):
-    """
-    Calculates scale factors for gL, gE, and gI for a conductance based synapse
-    tuning curve with maximum rate max_rate and x-intercept x_intercept.
+    Calculates scale and bias factors for gL, gE, and gI for a conductance based
+    synapse tuning curve with maximum rate max_rate and x-intercept x_intercept.
 
     max_rate: maximum rate of the tuning curve.
     x_intercept: x-intercept of the tuning curve.
     gL: if None, calculates a matching gL. Otherwise, keeps gL constant.
     """
 
+    bias_E, bias_I = 1, 1
     if gL is None:
-        gL, scale_E, scale_I, bias_E, bias_I = calc_gL_scale_E_scale_I(
-            x_intercept, max_rate, e_rev_E, e_rev_I, tau_ref), 1, 1
-    else:
-        scale_E, scale_I, bias_E, bias_I = optimize_scale_E_scale_I_bias_E_bias_I(
+        gL, scale_E, scale_I = calc_gL_scale_E_scale_I(
             x_intercept, max_rate, e_rev_E, e_rev_I, tau_ref)
+    else:
+        scale_E, scale_I, bias_I = calc_scale_E_scale_I_bias_I(
+            x_intercept, max_rate, gL, e_rev_E, e_rev_I, tau_ref)
+
+#        scale_E, scale_I, bias_E, bias_I = optimize_scale_E_scale_I_bias_E_bias_I(
+#            x_intercept, max_rate, gL, e_rev_E, e_rev_I, tau_ref)
 
     return gL, scale_E, scale_I, bias_E, bias_I
 
